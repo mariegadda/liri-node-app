9dc9ac6b9a6674d6694cc9072cbb38a423fcbf0b
Marie Gadda <mariegadda@gmail.com>
2017-04-26 20:07:55 -0400

    added node logic for twitter and spotify apis, spotify api almost working, not in right order yet.

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'sNqLA9C4ZR9Af3ePvmBAMyEnb',
+  consumer_secret: 'yJL6JsODks4OdSD8qKJb5Jbypex0d42OdHoE1nCHtGMPBeRa00',
+  access_token_key: '856204610175938560-gtkMsCmxOv5Gu47NH9RyveU3ES4qwa2',
+  access_token_secret: 'HQdUGJEmFpKFPtYq3DWn5Grz4uBMNO2qYjdDm4oCuHTtC',
+};
