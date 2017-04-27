607e683ce56bd1bd95eb53ab4e169b68ad656169
Marie Gadda <mariegadda@gmail.com>
2017-04-26 22:48:46 -0400

    finished OMDB portion, installed fs

--- liri.js
+++ liri.js
@@ -1,25 +1,26 @@
 // userRequest variable is what the user types into terminal to trigger the liri bot. 
 var userRequest = process.argv[2];
-
 // variable for the song or movie title 
 var title = process.argv[3];
+
+//===================================TWITTER==========================================
+
 //getting the data from keys.js
 var twitKeys = require('./keys.js');
 
 //using twitter module
 var Twitter = require('twitter');
 
-//making the client and pararms variable for twitter request
+//making the client and params variable for twitter request
 var client = new Twitter(twitKeys.twitterKeys);
 
 var params = {screen_name: 'CodePractice'};
-
 
 // request for twitter function
  if (userRequest === "my-tweets") {
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
  	 if (!error) {
-  		for (var i = 0; i < tweets.length; i++) {
+  		for (var i = 0; i < 20; i++) {
   			console.log(tweets[i].text);
   			console.log("Tweeted at:" + tweets[i].created_at);
   		} 
@@ -29,8 +30,12 @@ var params = {screen_name: 'CodePractice'};
 	});
   }
 
+// =============================SPOTIFY===============================================
+
 
 var spotify = require('spotify');
+
+
 // request for spotify function
 if (userRequest === "spotify-this-song" && typeof title !== 'undefined') {
     spotify.search({
@@ -39,36 +44,34 @@ if (userRequest === "spotify-this-song" && typeof title !== 'undefined') {
     }, function(err, data) {
         if (!err) {
 
-         	
-     // logs the artists (works for multiple artists for one song, first 3 songs)
+     // creates artist arrays in case one song has multiple artists, adds artist arrays to one array
            var artistArr = [];
            var bigArtistArr =[];
+			var music = data.tracks.items;
 
-			for (var i=0; i < data.tracks.items[0].artists.length; i++) {
-    		artistArr.push(data.tracks.items[0].artists[i].name);
+			for (var i=0; i < music[0].artists.length; i++) {
+    			artistArr.push(music[0].artists[i].name);
 				}
-			artistArr.join(', ');
+				artistArr.join(', ');
 
 				var artistArr1 = [];
-			for (var i=0; i < data.tracks.items[1].artists.length; i++) {
-    		artistArr1.push(data.tracks.items[1].artists[i].name);
+			for (var i=0; i < music[1].artists.length; i++) {
+    			artistArr1.push(music[1].artists[i].name);
 				}
-			artistArr1.join(', ');
+				artistArr1.join(', ');
 
 					var artistArr2 = [];
-			for (var i=0; i < data.tracks.items[2].artists.length; i++) {
-    		artistArr2.push(data.tracks.items[2].artists[i].name);
+			for (var i=0; i < music[2].artists.length; i++) {
+    			artistArr2.push(music[2].artists[i].name);
 				}
-			artistArr2.join(', ');
+				artistArr2.join(', ');
 
 		// an array of the artist name arrays (in case the song has mutliple artists)
 			bigArtistArr.push(artistArr, artistArr1, artistArr2);
 
  		// logs the artist name, title, line break, then the spotify url for that song
-
-           
             for (var i = 0; i < bigArtistArr.length; i++) {
-            	console.log(bigArtistArr[i] + ", " + data.tracks.items[i].album.name + '\n' + data.tracks.items[i].external_urls.spotify);
+            	console.log(' * ' + bigArtistArr[i] + ": "+ music[i].name + '\n' +  music[i].album.name + '\n' + music[i].external_urls.spotify);
             }
 
         } else {
@@ -98,28 +101,40 @@ if (userRequest === "spotify-this-song" && typeof title == 'undefined') {
 
     });
 }
-// node liri.js spotify-this-song '<song name here>'
-// This will show the following information about the song in your terminal/bash window
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
-// if no song is provided then your program will default to
-// "The Sign" by Ace of Base
-// node liri.js movie-this '<movie name here>'
-// This will output the following information to your terminal/bash window:
-//    * Title of the movie.
-//    * Year the movie came out.
-//    * IMDB Rating of the movie.
-//    * Country where the movie was produced.
-//    * Language of the movie.
-//    * Plot of the movie.
-//    * Actors in the movie.
-//    * Rotten Tomatoes URL.
-// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-// It's on Netflix!
+
+//=======================================OMDB=========================================
+
+
+var omdb = require('omdb');
+var request = require('request');
+
+if (userRequest === 'movie-this' && typeof title !== 'undefined') {
+	request("http://www.omdbapi.com/?t=" + title + "&plot=short&tomatoes=true&r=json", function(error, response, body){
+		var movie = JSON.parse(body);
+		console.log(movie.Title + '\n' + "Released in: " + movie.Year + '\n'+ 'IMDB Rating: ' + movie.imdbRating + '\n' +
+			'Produced in: ' + movie.Country + '\n' + 'Langauge: ' + movie.Language + '\n' + 'Plot: ' + movie.Plot + '\n' +
+			'Starring: '+ movie.Actors + '\n' + 'Rotten Tomatoes: ' + movie.tomatoURL);
+	});
+
+}if (userRequest === 'movie-this' && typeof title == 'undefined') {
+	request("http://www.omdbapi.com/?t=" + 'mr nobody' + "&plot=short&tomatoes=true&r=json", function(error, response, body){
+		var movie = JSON.parse(body);
+		console.log(movie.Title + '\n' + "Released in: " + movie.Year + '\n'+ 'IMDB Rating: ' + movie.imdbRating + '\n' +
+			'Produced in: ' + movie.Country + '\n' + 'Langauge: ' + movie.Language + '\n' + 'Plot: ' + movie.Plot + '\n' +
+			'Starring: '+ movie.Actors + '\n' + 'Rotten Tomatoes: ' + movie.tomatoURL);
+});
+}
+
+//===================================DO WHAT IT SAYS ========================================================
+	
+	
+
+
 // node liri.js do-what-it-says
 // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
 // Feel free to change the text in that document to test out the feature for other commands.
+
+
+
+
